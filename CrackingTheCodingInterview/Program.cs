5594f280155533568a9391dc38bd5fb68906d057
Shubham Patil <patilshubham131@gmail.com>
2022-06-05 18:07:26 +0530

    First commoit: check if the string has unique chars

---
+++ CrackingTheCodingInterview/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CrackingTheCodingInterview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. check if string contains Unique chars.
+            UniqueCharacterProblem ucp = new UniqueCharacterProblem();
+            Console.WriteLine(ucp.UniqueCharacters("shubham"));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CrackingTheCodingInterview/UniqueCharacterProblem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrackingTheCodingInterview
+{
+    class UniqueCharacterProblem
+    {
+        //solution to find if the string has the unique characters.
+
+        public bool UniqueCharacters(string input)
+        {
+            int checker = 0;
+
+            foreach(var ch in input)
+            {
+                int val = ch;
+
+                if ((checker & (1 << val)) > 0)
+                {
+                    return false;
+                }
+
+                checker |= (1<<val);
+            }
+
+            return true;
+        }
+    }
+}

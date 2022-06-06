bdd8013a9abfda9d45a77f8d9f48f5e1041de77e
Shubham Patil <patilshubham131@gmail.com>
2022-06-06 10:02:09 +0530

    problem 3: urlify string

---
+++ CrackingTheCodingInterview/CheckIfOneStringIsPermutationOfOther.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrackingTheCodingInterview
+{
+    class CheckIfOneStringIsPermutationOfOther
+    {
+
+        public bool isPermutation(string str1 , string str2)
+        {
+            var result1 =  performSort(str1);
+            var result2 = performSort(str2);
+            return result1 == result2;
+        }
+
+        private string performSort(string str)
+        {
+            char[] ch = str.ToCharArray();
+            Array.Sort(ch);
+            return new string(ch);
+        }
+    }
+}
--- CrackingTheCodingInterview/Program.cs
+++ CrackingTheCodingInterview/Program.cs
@@ -7,8 +7,14 @@ namespace CrackingTheCodingInterview
         static void Main(string[] args)
         {
             //1. check if string contains Unique chars.
-            UniqueCharacterProblem ucp = new UniqueCharacterProblem();
-            Console.WriteLine(ucp.UniqueCharacters("shubham"));
+            //UniqueCharacterProblem ucp = new UniqueCharacterProblem();
+            //Console.WriteLine(ucp.UniqueCharacters("shubham"));
+
+
+            //2. check if one string is permutation of other string.
+            CheckIfOneStringIsPermutationOfOther perm = new CheckIfOneStringIsPermutationOfOther();
+            Console.WriteLine(perm.isPermutation("abc", "bacd"));
+
 
             Console.ReadLine();
         }
